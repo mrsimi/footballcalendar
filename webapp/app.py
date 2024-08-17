68ab9b21f2421b5e8ef06b5507c8ce41a797eafd
Simi <similoluwa.adegoke@kuda.com>
2024-08-17 10:59:36 +0100

    update on fixtures

--- webapp/app.py
+++ webapp/app.py
@@ -14,10 +14,7 @@ def uploadForm():
         root_path = os.path.join(app.root_path, 'static/fixtures/')
         club_fixtures = get_fixtures(selected_teams, selected_competitions, root_path)
         file_download = create_fixtures_ics(club_fixtures, temp_dir.name)
-        print(selected_teams)
-        print(selected_competitions)
-        print(len(club_fixtures))
-        #file_download = create_ics_file('17/08 16:00', '17/08 17:00', 'Meeting with Team', 'Discuss project progress', 'meeting_event')
+        print(f'selected teams: {selected_teams} selected competitions: {selected_competitions} fixture {len(club_fixtures)}')
         return send_file(file_download, as_attachment=True)
 
     return render_template('index.html')
